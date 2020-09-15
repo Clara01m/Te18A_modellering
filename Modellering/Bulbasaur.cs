ecf739d49b6bf681bc0b93c61602e1ddc83036d5
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2020-09-15 13:33:55 +0200

    created a bulbasaur as an enemy so you can use anything within these classes in an actual game

---
+++ Modellering/Bulbasaur.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Modellering
+{
+    public class Bulbasaur
+    {
+        public float x2; 
+        public float y2; 
+        public int hp2 = 27; 
+        public string name2; 
+        public int attack2 = 5;
+
+    }
+}
