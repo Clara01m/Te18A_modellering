6f88649243d70d8c1ed6769401b0455af161293b
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2020-09-15 13:34:24 +0200

    made it look better and added attack

--- Modellering/Charmander.cs
+++ Modellering/Charmander.cs
@@ -4,9 +4,10 @@ namespace Modellering
 {
     public class Charmander
     {
-        public int hp= 25;
+        public int hp = 25;
         public float x; 
         public float y; 
+        public int attack = 6;
 
         public string name; 
 
