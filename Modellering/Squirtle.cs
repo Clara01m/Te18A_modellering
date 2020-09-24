67571437638be3ec3cd909c960d0ad9fb0df8411
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2020-09-24 16:29:26 +0200

    added a 3rd class and took away unecessairy code

---
+++ Modellering/Squirtle.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Modellering
+{
+    public class Squirtle
+    {
+        public float x3; 
+        public float y3; 
+        public int hp3 = 24; 
+        public string name3; 
+        public int attack3 = 6;
+}
+    }
+}
